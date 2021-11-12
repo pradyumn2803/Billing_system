a9fd31c2e45a7ba3959a9e94e149be52f97e0b1e
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2021-11-12 12:36:13 +0530

    modified code.c

--- code.c
+++ code.c
@@ -6,9 +6,9 @@
 int main()
 {
     int choice, quantity;
-    char order[1000], a[1000];
+    char order[1000];
     char c, e;
-    int amount = 0, i = 0, j = 0;
+    int amount = 0, i = 0, j = 0, flag = 0;
     int price[1000] = {77, 100, 43, 35, 64, 125, 151, 91, 118, 184, 27, 47, 25, 100, 50, 59, 45, 350, 370, 530, 395, 460, 430, 620, 150, 145, 96, 81, 54, 94, 579, 565, 165, 206, 193, 198, 849, 184, 165, 75, 165, 126, 210, 280, 210, 189, 199, 350, 478, 440, 215, 207, 309, 139, 248, 204, 195, 253, 175, 90, 175, 329, 175, 199};
     int Quantity[1000] = {100, 100, 100, 150, 150, 100, 100, 100, 100, 100, 50, 50, 50, 50, 50, 10, 10, 10, 10, 5, 5, 10, 10, 10, 5, 1000,
                           5, 5, 5, 5, 30, 30, 30, 30, 30, 30, 30, 30, 30, 10, 10, 300, 500, 500, 100, 100, 100, 100, 100, 100, 100, 100,
@@ -45,27 +45,19 @@ int main()
                 printf("What would you like to order: ");
                 scanf("%[^\n]%*c", order);
                 p = fopen("ITEM.txt", "r");
-                e = tolower(fgetc(p));
-                int k = 0;
-                while (1)
+                char a[1000];
+                while (fgets(a, sizeof(a), p))
                 {
-                    if (e == EOF)
+                    if (strcmp(order, a) == 0)
                     {
-                        printf("couldn't find \n");
-                        goto label;
-                    }
-                    while (e != '\n')
-                    {
-                        a[k++] = e;
-                        e = tolower(fgetc(p));
-                    }
-                    i++;
-                    k = 0;
-                    if (strcmp(a, order) == 0)
-                    {
+                        flag = 1;
                         break;
                     }
-                    e = tolower(fgetc(p));
+                    i++;
+                }
+                if (!flag)
+                {
+                    goto label;
                 }
                 fclose(p);
                 fflush(stdin);
